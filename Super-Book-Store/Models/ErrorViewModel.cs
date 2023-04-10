c00644a3993d5c978dbb98c0c61769bda24402c6
NHC612 <hcam418@gmail.com>
2023-04-10 19:10:27 +0700

    first commit

---
+++ Super-Book-Store/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Super_Book_Store.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
